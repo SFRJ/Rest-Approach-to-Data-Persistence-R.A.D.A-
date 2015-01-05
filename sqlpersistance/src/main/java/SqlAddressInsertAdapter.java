f2993af9b7a3c499d3706729aca75ef734ac2853
Djordje <someemail@someemailprovider.someextension>
2015-01-05 09:43:30 +0000

    removed comment

--- sqlpersistance/src/main/java/SqlAddressInsertAdapter.java
+++ sqlpersistance/src/main/java/SqlAddressInsertAdapter.java
@@ -40,7 +40,6 @@ public class SqlAddressInsertAdapter implements CreateService {
 
 
         session.save(ormAddress);
-//        session.save(ormPersons);
 
         session.getTransaction().commit();
     }
